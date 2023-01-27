209dc222a4089fba65d9af9fbdc5d5ecae5b6c2a
Michael Yarichuk <michael.yarichuk@gmail.com>
2023-01-28 01:32:57 +0200

    feat(git repo): implement git commit database for fetching commits (still needs testing)

---
+++ src/ChangelogGenerator.Api/GitCommitDatabase.cs
@@ -0,0 +1,79 @@
+﻿using LibGit2Sharp;
+
+namespace ChangelogGenerator.Api;
+
+public class GitCommitDatabase: IDisposable
+{
+    private bool _isDisposed;
+    private readonly Repository _repository;
+
+    public GitCommitDatabase(string repositoryPath)
+    {
+        ThrowIfInvalidRepo(repositoryPath);
+
+        _repository = new Repository(repositoryPath);
+        AssertRepositoryInGoodState();
+    }
+
+    public GitCommitDatabase(string repositoryPath, string username, string email)
+    {
+        ThrowIfInvalidRepo(repositoryPath);
+
+        _repository = new Repository(
+            repositoryPath,
+            new RepositoryOptions
+            {
+                Identity = new Identity(username, email)
+            });
+        AssertRepositoryInGoodState();
+    }
+
+    public IEnumerable<Commit> Query() => 
+        _repository.Commits.QueryBy(new CommitFilter());
+
+    public IEnumerable<Commit> Query(string startShaOrTag) => 
+        _repository.Commits.QueryBy(new CommitFilter
+        {
+            IncludeReachableFrom = startShaOrTag
+        });
+
+    public IEnumerable<Commit> Query(string startShaOrTag, string endShaOrTag) => 
+        _repository.Commits.QueryBy(new CommitFilter
+        {
+            IncludeReachableFrom = startShaOrTag,
+            ExcludeReachableFrom = endShaOrTag
+        });
+
+    private void AssertRepositoryInGoodState()
+    {
+        if (_repository.Info.CurrentOperation != CurrentOperation.None)
+        {
+            throw new RepositoryNotReadyException($"Repository is in the middle of a pending interactive operation. Cannot continue. (operation type = {_repository.Info.CurrentOperation})");
+        }
+    }
+
+    private static void ThrowIfInvalidRepo(string repositoryPath)
+    {
+        if (!Repository.IsValid(repositoryPath))
+        {
+            throw new ArgumentException($"The path '{repositoryPath}' is not a valid git repository");
+        }
+    }
+    
+    private void DisposeThis()
+    {
+        if (!_isDisposed)
+        {
+            _isDisposed = true;
+            _repository.Dispose();
+        }
+    }
+
+    public void Dispose()
+    {
+        DisposeThis();
+        GC.SuppressFinalize(this);
+    }
+
+    ~GitCommitDatabase() => Dispose();
+}
---
+++ src/ChangelogGenerator.Api/RepositoryNotReadyException.cs
@@ -0,0 +1,12 @@
+﻿namespace ChangelogGenerator.Api;
+
+public class RepositoryNotReadyException : Exception
+{
+    public RepositoryNotReadyException(string? message) : base(message)
+    {
+    }
+
+    public RepositoryNotReadyException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
