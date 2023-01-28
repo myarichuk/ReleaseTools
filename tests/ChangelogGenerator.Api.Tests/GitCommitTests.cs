448706b00e6b82c4cdf96a5c26450059f1623e6b
Michael Yarichuk <michael.yarichuk@gmail.com>
2023-01-28 08:53:34 +0200

    test: make sure the test folder for git commit database is deleted so test artifacts won't accumulate

--- tests/ChangelogGenerator.Api.Tests/GitCommitTests.cs
+++ tests/ChangelogGenerator.Api.Tests/GitCommitTests.cs
@@ -1,4 +1,6 @@
+using FluentAssertions;
 using LibGit2Sharp;
+using System.Runtime.InteropServices;
 using Xunit.Abstractions;
 
 namespace ChangelogGenerator.Api.Tests
@@ -9,22 +11,14 @@ namespace ChangelogGenerator.Api.Tests
         private readonly string _repositoryFolder;
         private readonly Repository _repository;
 
+        private static readonly Signature DefaultSignature = 
+            new("John Dow", "john.dow@foobar.com", DateTimeOffset.UtcNow);
+
         public GitCommitTests(ITestOutputHelper log)
         {
             _log = log;
 
-            var testRepoFolders = Directory.EnumerateDirectories("./", "test_repo_*", SearchOption.TopDirectoryOnly);
-            foreach (var testRepoFolder in testRepoFolders)
-            {
-                try
-                {
-                    Directory.Delete(testRepoFolder, true);
-                }
-                catch(Exception ex)
-                {
-                    _log.WriteLine(ex.ToString());
-                }
-            }
+            DeleteOldTestRepos();
 
             _repositoryFolder = $"test_repo_{Guid.NewGuid()}";
             Repository.Init(_repositoryFolder);
@@ -35,11 +29,87 @@ namespace ChangelogGenerator.Api.Tests
         public void Can_fetch_commits()
         {
             using var commitDb = new GitCommitDatabase(_repositoryFolder);
+
+            File.WriteAllText(Path.Combine(_repositoryFolder, "foobar1.txt"), "some file contents");
+            
+            Commands.Stage(_repository, "*");
+            _repository.Commit("commit A", DefaultSignature, DefaultSignature);
+
+            File.WriteAllText(Path.Combine(_repositoryFolder, "foobar2.txt"), "some file contents");
+            
+            Commands.Stage(_repository, "*");
+            _repository.Commit("commit B", DefaultSignature, DefaultSignature);
+
+            File.WriteAllText(Path.Combine(_repositoryFolder, "foobar3.txt"), "some file contents");
+            
+            Commands.Stage(_repository, "*");
+            _repository.Commit("commit C", DefaultSignature, DefaultSignature);
+            
+            var commitsQueryResult = commitDb.Query().ToArray();
+
+            commitsQueryResult.Should().HaveCount(3);
+            var commitMessages = commitsQueryResult.Select(x => x.Message);
+            commitMessages.Should().ContainInOrder("commit A\n", "commit B\n", "commit C\n");
         }
 
-        public void Dispose()
+        private void DeleteOldTestRepos()
         {
-            _repository.Dispose();
+            var testRepoFolders = Directory.EnumerateDirectories("./", "test_repo_*", SearchOption.TopDirectoryOnly);
+            foreach (var testRepoFolder in testRepoFolders)
+            {
+                try
+                {
+                    DeleteDirectory(testRepoFolder);
+                }
+                catch (Exception ex)
+                {
+                    _log.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        public void Dispose() => _repository.Dispose();
+
+        //credit: https://github.com/rgl/UseLibgit2sharp/blob/master/Program.cs
+        // recursively force the deletion of the given directory.
+        // NB on windows, because the git repository files are read-only,
+        //    Directory.Delete will fail with UnauthorizedAccessException,
+        //    so, before deleting a file, we have to remove its read-only
+        //    attribute.
+        private static void DeleteDirectory(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                return;
+            }
+
+            // on non-Windows use the regular Directory.Delete because they
+            // do not care about the file permissions. to delete a file,
+            // only the parent directory permissions matter.
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Directory.Delete(path, true);
+                return;
+            }
+
+            foreach (var directoryPath in Directory.GetDirectories(path))
+            {
+                DeleteDirectory(directoryPath);
+            }
+
+            foreach (var filePath in Directory.GetFiles(path))
+            {
+                var fileAttributes = File.GetAttributes(filePath);
+
+                if ((fileAttributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
+                {
+                    File.SetAttributes(filePath, fileAttributes ^ FileAttributes.ReadOnly);
+                }
+
+                File.Delete(filePath);
+            }
+
+            Directory.Delete(path);
         }
     }
 }
